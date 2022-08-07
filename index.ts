499d5345b7bca264244f6b43ace648981a8687b8
leflores <pflores.fisi22@gmail.com>
2022-08-06 22:00:59 -0500

    Implement printable word with colors

--- index.ts
+++ index.ts
@@ -7,15 +7,29 @@ let tries = 0;
 const previosGuesses: Array<string> = [];
 
 async function game() {
-    console.log(style.red("Welcome to deno-wordle!"));
-    const { Response: WORD_TO_GUESS } = await fetchWord();
+    console.log(style.brightMagenta("Welcome to deno-wordle!"));
+    const { Response: WORD_TO_GUESS } = await fetchWord() as { Response: string };
     console.log(`Secret: ${WORD_TO_GUESS}`);
 
     while (true) {
-        const userWord = prompt("Guess the word: ") as string;
+        const userWord = prompt("Guess the word:")?.toLowerCase() || "";
         const { error, valid } = validateWord(userWord);
 
         if (valid) {
+            // Printing word animation
+            for (const i in Array.from(userWord)) {
+                if (userWord[i] === WORD_TO_GUESS[i]) {
+                    console.log(style.bgGreen(` ${userWord[i]} `));
+                }
+                else {
+                    if (WORD_TO_GUESS.includes(userWord[i])) {
+                        console.log(style.bgYellow(` ${userWord[i]} `));
+                    }
+                    else {
+                        console.log(style.bgBrightBlack(` ${userWord[i]} `));
+                    }
+                }
+            }
             if (userWord === WORD_TO_GUESS) {
                 console.log("You win!");
                 break;
