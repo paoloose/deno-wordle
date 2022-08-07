b0edfbc729ec6599646e848363e219c414beeb2d
leflores <pflores.fisi22@gmail.com>
2022-08-07 00:11:41 -0500

    Implement animations with semantic colors

--- index.ts
+++ index.ts
@@ -1,46 +1,54 @@
 import * as style from "https://deno.land/std@0.146.0/fmt/colors.ts";
+import { formatWithColors, printWithAnimation } from "./printing.ts"
 
 const MAX_TRIES = 6;
 const WORD_LENGTH = 5;
-let tries = 0;
+let currentAttempt = 0;
 
-const previosGuesses: Array<string> = [];
+const previosGuesses: Array<Array<string>> = [];
+
+async function printAllAttempts(guesses: Array<Array<string>>) {
+    for (let i = 0; i < MAX_TRIES; i++) {
+        if (guesses[i] !== undefined) {
+            if (i == currentAttempt) await printWithAnimation(guesses[i]);
+            else console.log(guesses[i].join(" "));
+        }
+        else {
+            console.log(`${style.bgBlack("   ")} ${style.bgBlack("   ")} ${style.bgBlack("   ")} ${style.bgBlack("   ")} ${style.bgBlack("   ")}`);
+        }
+        console.log();
+    }
+}
 
 async function game() {
-    console.log(style.brightMagenta("Welcome to deno-wordle!"));
+
     const { Response: WORD_TO_GUESS } = await fetchWord() as { Response: string };
-    console.log(`Secret: ${WORD_TO_GUESS}`);
-
     while (true) {
-        const userWord = prompt("Guess the word:")?.toLowerCase() || "";
-        const { error, valid } = validateWord(userWord);
+        console.clear();
+        console.log(style.brightMagenta("Welcome to deno-wordle!"));
+        console.log(`Secret: ${WORD_TO_GUESS}`);
+        await printAllAttempts(previosGuesses);
+        const userWord = prompt(">")?.toLowerCase() || "";
 
+        const { error, valid } = validateWord(userWord);
+        
         if (valid) {
-            // Printing word animation
-            for (const i in Array.from(userWord)) {
-                if (userWord[i] === WORD_TO_GUESS[i]) {
-                    console.log(style.bgGreen(` ${userWord[i]} `));
-                }
-                else {
-                    if (WORD_TO_GUESS.includes(userWord[i])) {
-                        console.log(style.bgYellow(` ${userWord[i]} `));
-                    }
-                    else {
-                        console.log(style.bgBrightBlack(` ${userWord[i]} `));
-                    }
-                }
-            }
+            const attempt = formatWithColors(userWord, WORD_TO_GUESS);
+            previosGuesses.push(attempt);
+            console.clear();
+            console.log(style.brightMagenta("Welcome to deno-wordle!"));
+            console.log(`Secret: ${WORD_TO_GUESS}`);
+            await printAllAttempts(previosGuesses);
+            
             if (userWord === WORD_TO_GUESS) {
                 console.log("You win!");
                 break;
             }
-            tries++;
-            if (tries >= MAX_TRIES) {
+            currentAttempt++;
+            if (currentAttempt >= MAX_TRIES) {
                 console.log("You lose!");
                 break;
             }
-            previosGuesses.push(userWord);
-            console.log(`${tries} tries`);
         }
         else {
             console.log(error);
@@ -52,7 +60,7 @@ function validateWord(word: string): { error?: string, valid: boolean } {
 
     if (!word) return { error: "Empty word", valid: false };
     if (word.length !== WORD_LENGTH) return { error: "Not the length", valid: false };
-    if (previosGuesses.includes(word)) return { error: "Already tried", valid: false };
+    //if (previosGuesses.includes(word)) return { error: "Already tried", valid: false };
     if (!/^[a-zA-Z]+$/.test(word)) return { error: "Not a valid word", valid: false };
     return { error: "", valid: true };
 }
