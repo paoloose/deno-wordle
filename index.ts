d9e81f64be8686b95b2e51d56bc65b995b941cd8
leflores <pflores.fisi22@gmail.com>
2023-01-02 01:26:06 -0500

    use deno lint

--- index.ts
+++ index.ts
@@ -10,59 +10,59 @@ const previousGuesses: Array<Array<string>> = [];
 const previousPlainGuesses: Array<string> = [];
 
 async function printWordleUI() {
-    console.clear();
-    console.log(style.white("    W o r d l e\n"));
-    await printAllAttempts(previousGuesses, currentAttempt, MAX_TRIES);
+  console.clear();
+  console.log(style.white("    W o r d l e\n"));
+  await printAllAttempts(previousGuesses, currentAttempt, MAX_TRIES);
 }
 
 async function game() {
-    WORD_TO_GUESS = (await fetchWord()).Response;
-    let currentUserWord = "";
+  WORD_TO_GUESS = (await fetchWord()).Response;
+  let currentUserWord = "";
+
+  while (true) {
+
+    await printWordleUI();
+    if (currentUserWord === WORD_TO_GUESS) {
+      console.log(`You win! (${currentAttempt}/${MAX_TRIES})`);
+      break;
+    }
+    if (currentAttempt >= MAX_TRIES) {
+      console.log(`${style.gray("Was:")} ${style.italic(WORD_TO_GUESS)}`);
+      break;
+    }
 
     while (true) {
+      currentUserWord = prompt(">")?.toLowerCase() || "";
 
-        await printWordleUI();
-        if (currentUserWord === WORD_TO_GUESS) {
-            console.log(`You win! (${currentAttempt}/${MAX_TRIES})`);
-            break;
-        }
-        if (currentAttempt >= MAX_TRIES) {
-            console.log(`${style.gray("Was:")} ${style.italic(WORD_TO_GUESS)}`);
-            break;
-        }
-        
-        while (true) {
-            currentUserWord = prompt(">")?.toLowerCase() || "";
-            
-            const { error, valid } = validateWord(currentUserWord);
-            
-            if (valid) {
-                previousPlainGuesses.push(currentUserWord);
-                const attempt = formatWithColors(currentUserWord, WORD_TO_GUESS);
-                previousGuesses.push(attempt);
-                currentAttempt++;
-                break;
-            }
-            else {
-                console.log(style.gray(error!));
-            }
-        }
+      const { error, valid } = validateWord(currentUserWord);
+
+      if (valid) {
+        previousPlainGuesses.push(currentUserWord);
+        const attempt = formatWithColors(currentUserWord, WORD_TO_GUESS);
+        previousGuesses.push(attempt);
+        currentAttempt++;
+        break;
+      }
+      else {
+        console.log(style.gray(error!));
+      }
     }
+  }
 }
 
 function validateWord(word: string): { error?: string, valid: boolean } {
-    if (!word) return { error: "Empty word", valid: false };
-    if (word.length !== WORD_LENGTH) return { error: "Not the length", valid: false };
-    if (previousPlainGuesses.includes(word)) return { error: "Already tried", valid: false };
-    if (!/^[a-zA-Z]+$/.test(word)) return { error: "Not a valid word", valid: false };
-    return { error: "", valid: true };
+  if (!word) return { error: "Empty word", valid: false };
+  if (word.length !== WORD_LENGTH) return { error: "Not the length", valid: false };
+  if (previousPlainGuesses.includes(word)) return { error: "Already tried", valid: false };
+  if (!/^[a-zA-Z]+$/.test(word)) return { error: "Not a valid word", valid: false };
+  return { error: "", valid: true };
 }
 
 async function fetchWord(): Promise<{ Response: string }> {
-    console.log(style.brightBlack("Fetching word..."));
-    const response = fetch("https://thatwordleapi.azurewebsites.net/get/");
-    const res = await response;
-    return await res.json();
+  console.log(style.brightBlack("Fetching word..."));
+  const response = fetch("https://thatwordleapi.azurewebsites.net/get/");
+  const res = await response;
+  return await res.json();
 }
 
 game();
--- printing.ts
+++ printing.ts
@@ -3,115 +3,115 @@ import { writeAllSync } from "conversion";
 import { WORD_LENGTH } from "./index.ts"
 
 function waitMilliseconds(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function printChar(char: string) {
-    const contentBytes = new TextEncoder().encode(char);
-    // We use the `writeSync` method to write encoded bytes to the terminal
-    // The only way I found to print without a new line
-    await waitMilliseconds(200).then(() => {
-        writeAllSync(Deno.stdout, contentBytes);
-    });
+  const contentBytes = new TextEncoder().encode(char);
+  // We use the `writeSync` method to write encoded bytes to the terminal
+  // The only way I found to print without a new line
+  await waitMilliseconds(200).then(() => {
+    writeAllSync(Deno.stdout, contentBytes);
+  });
 }
 
 export async function printWithAnimation(wordlist: Array<string>) {
-    for (const word of wordlist) {
-        await printChar(word + " ");
-    }
-    console.log(); // new line
+  for (const word of wordlist) {
+    await printChar(word + " ");
+  }
+  console.log(); // new line
 }
 
 export async function printAllAttempts(guesses: Array<Array<string>>, currentAttempt: number, maxAttempts: number) {
-    for (let i = 0; i < maxAttempts; i++) {
-        if (guesses[i] !== undefined) {
-            if (i+1 == currentAttempt) {
-                await printWithAnimation(guesses[i]);
-            }
-            else console.log(guesses[i].join(" "));
-        }
-        else {
-            let empty = "";
-            for (let i = 0; i < WORD_LENGTH; i++) {
-                empty += `${style.bgBlack("   ")} `;
-            }
-            console.log(empty);
-        }
-        console.log();
+  for (let i = 0; i < maxAttempts; i++) {
+    if (guesses[i] !== undefined) {
+      if (i + 1 == currentAttempt) {
+        await printWithAnimation(guesses[i]);
+      }
+      else console.log(guesses[i].join(" "));
     }
+    else {
+      let empty = "";
+      for (let i = 0; i < WORD_LENGTH; i++) {
+        empty += `${style.bgBlack("   ")} `;
+      }
+      console.log(empty);
+    }
+    console.log();
+  }
 }
 
 // 🟨🟩⬛️ Wordle Semantic colors algorithm 🟨🟩⬛️
 
 type Dictionary = {
-    [Key: string]: number;
+  [Key: string]: number;
 }
 const initializateDictonaryToZero = (dict: Dictionary, list: Array<string>) => {
-    for (const element of list) {
-        dict[element] = 0;
-    }
+  for (const element of list) {
+    dict[element] = 0;
+  }
 };
 const countToDictionary = (dict: Dictionary, key: string) => {
-    if (dict[key]) dict[key]++;
-    else dict[key] = 1;
+  if (dict[key]) dict[key]++;
+  else dict[key] = 1;
 };
 
 export function formatWithColors(userWord: string, toGuess: string) {
-    const formattedWord: Array<string> = [];
+  const formattedWord: Array<string> = [];
 
-    // To emulate the original Wordle algorithm, we need three dictionaries:
+  // To emulate the original Wordle algorithm, we need three dictionaries:
 
-    // To count the ocurrences of each letter in the Answer-word
-    const ocurrencesInAnswer: Dictionary = {};
-    initializateDictonaryToZero(ocurrencesInAnswer, Array.from(userWord));
-    for (const i in Array.from(toGuess)) {
-        countToDictionary(ocurrencesInAnswer, toGuess[i]);
+  // To count the ocurrences of each letter in the Answer-word
+  const ocurrencesInAnswer: Dictionary = {};
+  initializateDictonaryToZero(ocurrencesInAnswer, Array.from(userWord));
+  for (const i in Array.from(toGuess)) {
+    countToDictionary(ocurrencesInAnswer, toGuess[i]);
+  }
+
+  // Initialize dictionary values to 0
+  // To have the count of the letters that we are printing
+  const ocurrencesPrinted: Dictionary = {};
+  initializateDictonaryToZero(ocurrencesPrinted, Array.from(userWord));
+
+  // To count the ocurrences of matched green letters
+  const ocurrencesMatched: Dictionary = {};
+  initializateDictonaryToZero(ocurrencesMatched, Array.from(userWord));
+  for (const i in Array.from(userWord)) {
+    if (toGuess[i] === userWord[i]) {
+      countToDictionary(ocurrencesMatched, userWord[i]);
     }
+  }
 
-    // Initialize dictionary values to 0
-    // To have the count of the letters that we are printing
-    const ocurrencesPrinted: Dictionary = {};
-    initializateDictonaryToZero(ocurrencesPrinted, Array.from(userWord));
+  // Printing
+  for (const i in Array.from(userWord)) {
+    let formattedChar: string;
+    const currentChar = userWord[i];
 
-    // To count the ocurrences of matched green letters
-    const ocurrencesMatched: Dictionary = {};
-    initializateDictonaryToZero(ocurrencesMatched, Array.from(userWord));
-    for (const i in Array.from(userWord)) {
-        if (toGuess[i] === userWord[i]) {
-            countToDictionary(ocurrencesMatched, userWord[i]);
-        }
+    // Conditions to print it in green:
+    // 1. The letter is in the Answer-word at the same position
+    if (currentChar === toGuess[i]) {
+      formattedChar = style.bgGreen(` ${currentChar} `);
+      countToDictionary(ocurrencesPrinted, currentChar);
     }
-    
-    // Printing
-    for (const i in Array.from(userWord)) {
-        let formattedChar: string;
-        const currentChar = userWord[i];
+    else {
+      // Conditions to print it in yellow:
+      // 1. The letter is not green
+      // 2. The letter appears in the ocurrencesInAnswer dictionary
+      // 3. The times the user green-matched the letter is less than the times that letter appears in the answer
+      // 4. The times we printed the letter is less than the times that letter appears in the answer
+      if (ocurrencesMatched[currentChar] < ocurrencesInAnswer[currentChar]
+        && ocurrencesPrinted[currentChar] < ocurrencesInAnswer[currentChar]) {
 
-        // Conditions to print it in green:
-        // 1. The letter is in the Answer-word at the same position
-        if (currentChar === toGuess[i]) {
-            formattedChar = style.bgGreen(` ${currentChar} `);
-            countToDictionary(ocurrencesPrinted, currentChar);
-        }
-        else {
-            // Conditions to print it in yellow:
-            // 1. The letter is not green
-            // 2. The letter appears in the ocurrencesInAnswer dictionary
-            // 3. The times the user green-matched the letter is less than the times that letter appears in the answer
-            // 4. The times we printed the letter is less than the times that letter appears in the answer
-            if (ocurrencesMatched[currentChar] < ocurrencesInAnswer[currentChar]
-                && ocurrencesPrinted[currentChar] < ocurrencesInAnswer[currentChar]) {
-
-                formattedChar = style.bgYellow(` ${currentChar} `);
-                countToDictionary(ocurrencesPrinted, currentChar);
-            }
-            // Conditions to print it in gray:
-            // 1. The letter is neither green nor yellow
-            else {
-                formattedChar = style.bgBrightBlack(` ${currentChar} `);
-            }
-        }
-        formattedWord.push(formattedChar);
+        formattedChar = style.bgYellow(` ${currentChar} `);
+        countToDictionary(ocurrencesPrinted, currentChar);
+      }
+      // Conditions to print it in gray:
+      // 1. The letter is neither green nor yellow
+      else {
+        formattedChar = style.bgBrightBlack(` ${currentChar} `);
+      }
     }
-    return formattedWord;
+    formattedWord.push(formattedChar);
+  }
+  return formattedWord;
 }
